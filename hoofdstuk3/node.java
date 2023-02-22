521ccc71372e43422616ca656f1d7c97a3e2dbde
jey-ou <83501213+jey-ou@users.noreply.github.com>
2023-02-22 12:18:47 +0100

    Update node.java

--- hoofdstuk3/node.java
+++ hoofdstuk3/node.java
@@ -1,1 +1,12 @@
 // test
+import java.util* ; // na te zien
+
+class Node{
+  ArrayList<Node> nodes = new ArrayList<>();  
+  
+  public Node (){}; // constructor
+
+  public Node addNode(Node n){
+    nodes.add(n);
+  }
+}

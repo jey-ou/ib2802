353b75a776e82bea278ca7dfe00e15e47285ddf8
jey-ou <83501213+jey-ou@users.noreply.github.com>
2023-02-22 12:43:48 +0100

    Update node.java

--- hoofdstuk3/node.java
+++ hoofdstuk3/node.java
@@ -1,12 +1,24 @@
 // test
 import java.util* ; // na te zien
 
-class Node{
-  ArrayList<Node> nodes = new ArrayList<>();  
+class T Node{
+  ArrayList<Node> nodes = new ArrayList<>();  // voor tree graph
+  T label;
   
-  public Node (){}; // constructor
+  public Node (T label){
+    this.label = label;
+  }; // constructor
 
-  public Node addNode(Node n){
+  public void addNode(Node n){ // kan niet voor node wel voor tree, graph
     nodes.add(n);
+  }
+  public void deleteNode(Node n){ //kan niet voor node wel voor tree, graph
+    nodes.remove(n);
+  }
+  public boolean containNode(Node n){ //kan niet voor node wel voor tree, graph
+    nodes.contain(n); 
+  }
+  public ArrayList<Node> getNodelist(){ //kan niet voor node wel voor tree, graph
+    return nodes; 
   }
 }

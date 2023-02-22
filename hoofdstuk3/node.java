1e571df04ab2dc0b72434eac4f6ec3917448a5cb
jey-ou <83501213+jey-ou@users.noreply.github.com>
2023-02-22 18:19:44 +0100

    Update node.java

--- hoofdstuk3/node.java
+++ hoofdstuk3/node.java
@@ -6,16 +6,26 @@ class Node{
   int value; // heuristic value
   Node node; // voor een graph
   
+  public Node(){}
   public Node (String label){
     this.label = label;
-  }; // constructor
-  public Node (String Label, int value) {
+  } // constructor
+  public Node (String label, int value) {
     this.label = label;
     this.value = value;
   }
   
-  public void changeValue(int value){
+  public bool changeValue(int value){
     this.value = value;
+    return true;
   }
 
+  public bool changeLabel(String label){
+    this label = label;
+    return true;
+  }
+  
+  public bool changeNode(Node node){
+    this.node = node;
+    return true;
 }

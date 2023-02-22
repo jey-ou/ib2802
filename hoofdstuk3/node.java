09d4ccb2a105d40023f2ba40008e099ca813b562
jey-ou <83501213+jey-ou@users.noreply.github.com>
2023-02-22 18:02:20 +0100

    Update node.java

--- hoofdstuk3/node.java
+++ hoofdstuk3/node.java
@@ -2,7 +2,6 @@
 import java.util* ; // na te zien
 
 class Node{
-  //ArrayList<Node> nodes = new ArrayList<>();  // voor tree graph
   String label;
   int value;
   
@@ -13,18 +12,5 @@ class Node{
     this.label = label;
     this.value = value;
   }
-  /**
-  public void addNode(Node n){ // kan niet voor node wel voor tree, graph
-    nodes.add(n);
-  }
-  public void deleteNode(Node n){ //kan niet voor node wel voor tree, graph
-    nodes.remove(n);
-  }
-  public boolean containNode(Node n){ //kan niet voor node wel voor tree, graph
-    nodes.contain(n); 
-  }
-  public ArrayList<Node> getNodelist(){ //kan niet voor node wel voor tree, graph
-    return nodes; 
-  }
-  **/
+
 }
